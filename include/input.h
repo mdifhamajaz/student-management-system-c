b1047f603f51f07f706473f8e08f333ca3eae817
Md Ifham Ajaz <devifham@gmail.com>
2026-07-08 15:38:00 +0530

    refactor: remove clearInputBuffer function in preparation for fgets migration

--- include/input.h
+++ include/input.h
@@ -1,7 +1,6 @@
 #ifndef INPUT_H
 #define INPUT_H
 
-void clearInputBuffer(void); //TODO: remove after scanf changed to fgets
 void readString(const char *prompt, char *buffer, int size);
 int readInt(const char *prompt);
 float readFloat(const char *prompt);
--- src/input.c
+++ src/input.c
@@ -4,15 +4,6 @@
 
 #include "input.h"
 
-void clearInputBuffer(void)
-{
-    int ch;
-
-    while ((ch = getchar()) != '\n' && ch != EOF)
-        ;
-}
-// TODO: this above function will be removed when all scanf converted to fgets.
-
 void readString(const char *prompt, char *buffer, int size)
 {
     printf("%s", prompt);
