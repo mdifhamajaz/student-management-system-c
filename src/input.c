dfadc7e1729f1c7c36cc952b2eb4f985218b6ec8
Md Ifham Ajaz <devifham@gmail.com>
2026-07-08 15:58:23 +0530

    Complete input system refactor with validated fgets-based input

--- src/input.c
+++ src/input.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
+#include <limits.h>
+#include <float.h>
 
 #include "input.h"
 
@@ -20,7 +23,18 @@ int readInt(const char *prompt)
     while (1)
     {
         readString(prompt, str, sizeof(str));
+        errno = 0;
         value = strtol(str, &endPtr, 10);
+        if (errno == ERANGE)
+        {
+            printf("Number out of range.\n");
+            continue;
+        }
+        if (value > INT_MAX || value < INT_MIN)
+        {
+            printf("Number is outside int range.\n");
+            continue;
+        }
         if (endPtr != str && *endPtr == '\0')
         {
             return (int)value;
@@ -38,12 +52,23 @@ float readFloat(const char *prompt)
     while (1)
     {
         readString(prompt, str, sizeof(str));
+        errno = 0;
         value = strtod(str, &endPtr);
+        if (errno == ERANGE)
+        {
+            printf("Number out of range.\n");
+            continue;
+        }
+        if (value > FLT_MAX || value < -FLT_MAX)
+        {
+            printf("Number is outside float range.\n");
+            continue;
+        }
 
-        if(endPtr != str && *endPtr == '\0') {
+        if (endPtr != str && *endPtr == '\0')
+        {
             return (float)value;
         }
         printf("Invalid input. Please try again.\n");
-
     }
 }
